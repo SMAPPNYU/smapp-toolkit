efe3e2a918bb935bf742cc10f7bfc1da7fdeef11
Jonathan Ronen <yablee@gmail.com>
2014-11-05 15:24:24 -0500

    use version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='smapp-toolkit',
-      version='dev',
+      version='0.1.1',
       description='NYU SMaPP lab toolkit',
       author='NYU SMaPP',
       license='GPLv2',
--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+sed -r "s/version='([0-9]\.[0-9]\.)([0-9]+)'/echo '      version=\'\1\$((\2+1))\''/ge" setup.py > setup.py
+git commit -am "Bump version number"
 python setup.py sdist upload

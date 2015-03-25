e52fd9af32492b433c7850363baa8a5d31776b6b
dpenfoldbrown <dpenfoldbrown@gmail.com>
2015-03-25 16:23:25 -0400

    Removed conditional import for figures

--- smapp_toolkit/twitter/__init__.py
+++ smapp_toolkit/twitter/__init__.py
@@ -1,12 +1,7 @@
 """
 Module indicator
 """
-try:
-    import twitter_figure_makers
-    NO_FIGURES = False
-except:
-    warnings.warn("smapp-toolkit: Missing some graphics packages. Making figures won't work.\n")
-    NO_FIGURES = True
 
+import figure_makers
 from mongo_tweet_collection import MongoTweetCollection
 from bson_tweet_collection import BSONTweetCollection

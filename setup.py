04deb39823e6ad0b35569c7b6d28ba9020d745c0
dpenfoldbrown <dpenfoldbrown@gmail.com>
2015-03-25 20:08:10 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='smapp-toolkit',
-      version='0.1.15',
+      version='0.1.16',
       description='NYU SMaPP lab toolkit',
       author='NYU SMaPP',
       license='GPLv2',

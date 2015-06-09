7e7ecfa62af0d8f19fd459089e744626adb78bbc
Jonathan Ronen <yablee@gmail.com>
2015-06-09 18:02:29 -0400

    Fix example script a little

--- examples/locations_by_timeunit.py
+++ examples/locations_by_timeunit.py
@@ -8,10 +8,14 @@ from datetime import datetime, timedelta
 from smapp_toolkit.twitter import MongoTweetCollection
 import matplotlib.pyplot as plt
 
-col = MongoTweetCollection('smapp.politics.fas.nyu.edu', 27011, 'smapp_readOnly', 'SECRET-PASSWORD', 'Britain_Geo')
+plt.ion()
 
-col.geolocation_names_per_day_figure(start=datetime(2015,1,12,17,34), step_size=timedelta(minutes=10), num_steps=6, n_names=8, xtick_format='%H:%M')
+col = MongoTweetCollection('smapp.politics.fas.nyu.edu', 27011, 'smapp_readOnly', 'PASSWORD', 'Britain_Geo')
+
+plt.figure(figsize=(10,6))
+col.geolocation_names_per_day_figure(start=datetime(2015,1,12,17,34), step_size=timedelta(minutes=10), num_steps=6, n_names=5, xtick_format='%H:%M')
 plt.savefig('geolocation_names.png')
 
+plt.figure(figsize=(10,6))
 col.user_locations_per_day_figure(start=datetime(2015,1,12,17,34), step_size=timedelta(minutes=10), num_steps=6, n_names=8, xtick_format='%H:%M')
 plt.savefig('user_locations.png')

5016f4d4ee2de86591099b94a5f2ba3fe505ec94
yvan <yvanscher@gmail.com>
2016-01-13 16:48:29 -0500

    yvanbump.sh added

--- pushpypi.sh
+++
@@ -1,9 +0,0 @@
-# step one extract the version # from the old file
-# step two increment the version number
-# step three commit it
-# step 4 push to pypi
-
-# old script used sed which doesn't port to other systems - options change. often broke, 
-# wiped the file, and didn't push new versions to pypi.
-
-# we should use twine instead of standard upload to pypi, standard upload only uses TLS in later versions.
--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# yvan
+
+# yvanbump.sh is the new script
+# old script used sed which doesn't port to other systems - options change. often broke, 
+# wiped the file, and didn't push new versions to pypi.
+
+# we should use twine instead of standard upload to pypi, standard upload only uses TLS in later versions.
 
 # bump and push to git
 bash yvanbump.sh
